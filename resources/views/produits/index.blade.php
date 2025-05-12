e2378b501f8ee139469918b6d83f08354b592a17
Zouine2005 <zouinemohamade@gmail.com>
2025-05-12 18:55:45 +0100

    Enhance produit export functionality with search parameter and reset option

--- resources/views/produits/index.blade.php
+++ resources/views/produits/index.blade.php
@@ -7,34 +7,35 @@
         <div class="d-flex justify-content-between align-items-center mb-4">
             <h2 class="mb-0">Liste des Produits</h2>
             <div class="d-flex gap-2">
-            <div class="dropdown">
-                    <button class="btn btn-outline-secondary d-flex align-items-center gap-2" type="button" id="exportDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
-                            <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
-                            <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
-                        </svg>
-                        Exporter
-                    </button>
-                    <ul class="dropdown-menu" aria-labelledby="exportDropdown">
-                        <li>
-                            <a class="dropdown-item" href="{{ route('produits.export.excel') }}">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-excel me-2" viewBox="0 0 16 16">
-                                    <path d="M5.884 6.68a.5.5 0 1 0-.768-.64L4 7.22l-1.116-1.18a.5.5 0 0 0-.768.64L3.629 8.2 2.116 9.72a.5.5 0 0 0 .768.64L4 9.18l1.116 1.18a.5.5 0 0 0 .768-.64L4.371 8.2l1.513-1.52z"/>
-                                    <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z"/>
-                                </svg>
-                                Exporter en Excel
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="{{ route('produits.export.pdf') }}">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-pdf me-2" viewBox="0 0 16 16">
-                                    <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 915V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2A1.5 1.5 0 0 0 11 4.5h2v9.255a2.336 2.336 0 0 1-.331-.15c-.345.375-.678.683-.975.912a7.321 7.321 0 0 1-1.891.76c-.79.233-1.6.291-2.377.145-.805-.151-1.6-.45-2.378-1.07A6.643 6.643 0 0 1 3 12.11a6.212 6.212 0 0 1-.532-2.745 6.1 6.1 0 0 1 .763-2.937 6.42 6.42 0 0 1 2.07-2.07 5.999 5.999 0 0 1 2.938-.763 6.42 6.42 0 0 1 2.745.532c.627.27 1.2.645 1.708 1.108.496.45.945.975 1.332 1.552.151.225.292.457.414.695zm-6.285-1.575c-.342.06-.69.093-1.037.093-1.072 0-1.994-.698-2.307-1.663-.15.075-.307.135-.468.18a4.123 4.123 0 0 1-1.125.12c-.375 0-.75-.045-1.125-.165-.36-.12-.69-.285-1.005-.51a2.704 2.704 0 0 1-.75-.84c-.135-.255-.24-.525-.315-.81-.045-.15-.075-.315-.075-.48 0-.585.21-1.125.585-1.545.36-.405.855-.72 1.455-.915.585-.18 1.245-.27 1.905-.27.66 0 1.29.09 1.875.27.585.18 1.08.495 1.44.915.375.42.585.96.585 1.545 0 .165-.03.33-.075.495-.075.285-.18.555-.315.81-.135.27-.345.495-.615.69-.27.18-.585.315-.945.39zm1.95-3.885c-.27-.405-.645-.765-1.08-1.035a4.668 4.668 0 0 0-1.56-.585c-.54-.09-1.095-.135-1.665-.135-.57 0-1.125.045-1.665.135-.54.09-1.05.27-1.515.585-.465.315-.855.675-1.125 1.095-.135.21-.255.435-.345.675-.045.12-.075.24-.075.375 0 .51.195.975.585 1.32.39.33.885.585 1.455.735.585.135 1.2.195 1.815.195.615 0 1.2-.06 1.755-.195.555-.15 1.035-.405 1.395-.735.39-.345.585-.81.585-1.32 0-.135-.03-.255-.075-.375-.09-.24-.21-.465-.345-.675z"/>
-                                </svg>
-                                Exporter en PDF
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+                        <div class="dropdown">
+                <button class="btn btn-outline-secondary d-flex align-items-center gap-2 animate-btn" type="button" id="exportDropdown" data-bs-toggle="dropdown" aria-expanded="false" data-bs-toggle="tooltip" data-bs-placement="top" title="Exporter la liste des produits">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+                        <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+                        <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+                    </svg>
+                    Exporter
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="exportDropdown">
+                    <li>
+                        <a class="dropdown-item" href="{{ route('produits.export.excel', ['search' => request('search')]) }}" onclick="showLoader()">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-excel me-2" viewBox="0 0 16 16">
+                                <path d="M5.884 6.68a.5.5 0 1 0-.768-.64L4 7.22l-1.116-1.18a.5.5 0 0 0-.768.64L3.629 8.2 2.116 9.72a.5.5 0 0 0 .768.64L4 9.18l1.116 1.18a.5.5 0 0 0 .768-.64L4.371 8.2l1.513-1.52z"/>
+                                <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z"/>
+                            </svg>
+                            Exporter en Excel
+                        </a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="{{ route('produits.export.pdf', ['search' => request('search')]) }}" onclick="showLoader()">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-pdf me-2" viewBox="0 0 16 16">
+                                <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z"/>
+                                <path d="M4.603 14.087a.81.81 0 0 1-.438-.42c-.195-.388-.13-.776.08-1.102.198-.307.526-.568.897-.787a7.68 7.68 0 0 1 1.482-.645 19.697 19.697 0 0 0 1.062-2.227 7.269 7.269 0 0 1-.43-1.295c-.086-.4-.119-.796-.046-1.136.075-.354.274-.672.65-.823.192-.077.4-.12.602-.077a.7.7 0 0 1 .477.365c.088.164.12.356.127.538.007.188-.012.396-.047.614-.084.51-.27 1.134-.52 1.794a10.954 10.954 0 0 0 .98 1.686 5.753 5.753 0 0 1 1.334.05c.364.054.437.232.43.41-.007.194-.087.372-.277.521-.176.134-.374.192-.577.192-.177 0-.335-.018-.492-.065a9.016 9.016 0 0 1-1.168-.438 20.66 20.66 0 0 1-1.002 2.114 8.165 8.165 0 0 1-1.229.637c-.388.13-.804.162-1.198.08z"/>
+                            </svg>
+                            Exporter en PDF
+                        </a>
+                    </li>
+                </ul>
+            </div>
             <a href="{{ route('produits.create') }}" class="btn btn-primary d-flex align-items-center gap-2 animate-btn" data-bs-toggle="tooltip" data-bs-placement="top" title="Ajouter un produit">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
@@ -63,6 +64,14 @@
                 </span>
                 <input type="text" name="search" value="{{ request('search') }}" class="form-control" placeholder="Rechercher par numéro, désignation ou société" aria-label="Recherche de produits">
                 <button type="submit" class="btn btn-outline-secondary animate-btn" data-bs-toggle="tooltip" data-bs-placement="top" title="Rechercher">Rechercher</button>
+                @if (request('search'))
+                            <a href="{{ route('produits.index') }}" class="btn btn-outline-secondary animate-btn" data-bs-toggle="tooltip" data-bs-placement="top" title="Réinitialiser la recherche">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                                </svg>
+                            </a>
+                        @endif
             </div>
         </form>
 
